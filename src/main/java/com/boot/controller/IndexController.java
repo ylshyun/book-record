00a4660fe341ecd1de9c8976d275076108586f6e
yysh128 <sh1831083@gmail.com>
2025-05-30 00:37:42 +0900

    index pagination modify

--- src/main/java/com/boot/controller/IndexController.java
+++ src/main/java/com/boot/controller/IndexController.java
@@ -20,9 +20,32 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model, @RequestParam(defaultValue = "0") int page) {
         Page<ReviewBookDTO> reviews = reviewService.findAllReviews(PageRequest.of(page, 10, Sort.by("modifiedDate").descending()));
+        int totalPages = reviews.getTotalPages();   // 전체 페이지 수
+        int currentPage = reviews.getNumber();  // 현재 페이지 번호
+
+        int displayPages = 5;
+        int startPage;
+        int endPage;
+
+        if (totalPages <= displayPages) {
+            startPage = 0;
+            endPage = totalPages - 1;
+        } else if (currentPage <= 2) {
+            startPage = 0;
+            endPage = displayPages - 1;
+        } else if (currentPage >= totalPages - 3){
+            startPage = totalPages - displayPages;
+            endPage = totalPages - 1;
+        } else{
+            startPage = currentPage -2;
+            endPage = currentPage + 2;
+        }
+
         model.addAttribute("reviews", reviews);
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", reviews.getTotalPages());
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("totalPages", totalPages);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
         return "index";
     }
 }
