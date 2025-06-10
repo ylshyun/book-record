929cd97aa278506d94b68ddbfbc8622b4e0bed68
yysh128 <sh1831083@gmail.com>
2025-06-10 21:59:43 +0900

    test data initializer

---
+++ src/main/java/com/boot/config/DataInitializer.java
@@ -0,0 +1,69 @@
+package com.boot.config;
+
+import com.boot.dto.BookDTO;
+import com.boot.entity.Book;
+import com.boot.entity.Member;
+import com.boot.entity.Review;
+import com.boot.repository.BookRepository;
+import com.boot.repository.MemberRepository;
+import com.boot.repository.ReviewRepository;
+import com.boot.service.BookService;
+import com.boot.service.NaverApi;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Profile("dev")
+@RequiredArgsConstructor
+public class DataInitializer implements ApplicationRunner {
+
+    private final MemberRepository memberRepository;
+    private final ReviewRepository reviewRepository;
+    private final BookService bookService;
+    private final NaverApi naverApi;
+    private final PasswordEncoder passwordEncoder;
+    private final BookRepository bookRepository;
+
+    public void run(ApplicationArguments args) throws Exception {
+        List<String> keywords = List.of("자바", "알고리즘", "스프링부트", "jpa", "데이터베이스","java","database");
+
+        for (String keyword : keywords) {
+            List<BookDTO> apiBooks = naverApi.searchBooksByKeyword("%" + keyword + "%");
+            bookService.saveBooksIfNotExist(apiBooks);
+        }
+
+        if (memberRepository.count() < 50) {
+            for (int i = 0; i < 50; i++) {
+                memberRepository.save(Member.builder()
+                        .memberName("member" + i)
+                        .memberEmail("memberEmail" + i + "@gmail.com")
+                        .memberPassword(passwordEncoder.encode("memberPassword" + i))
+                        .role("ROLE_MEMBER")
+                        .build());
+            }
+        }
+        if (reviewRepository.count() == 0) {
+            List<Book> books = bookRepository.findAll();
+            List<Member> members = memberRepository.findAll();
+
+            for (int i = 0; i < 100; i++) {
+                Book book = books.get(i % books.size());
+                Member member = members.get(i % members.size());
+
+                reviewRepository.save(Review.builder()
+                        .member(memberRepository.findByMemberEmail("memberEmail" + i + "@gmail.com"))
+                        .reviewContent("이 리뷰는 테스트용으로 작성되었으며, 리뷰의 내용은 50자 이상이어야 합니다. " + i)
+                        .reviewTitle("리뷰 제목 " + i)
+                        .book(book)
+                        .member(member)
+                        .build());
+            }
+        }
+    }
+}
